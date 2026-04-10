44dae06792f561e043cf11996b84b8b32e5aeaa4
carlosacostap-unca <carlosacostap-unca@users.noreply.github.com>
2026-04-10 17:09:46 -0300

    Eliminar scripts de migración temporales para solucionar error de compilación en Dokploy (Nixpacks UTF-8)

--- temp.js
+++
@@ -1,7 +0,0 @@
-async function checkSchema() {
-  const res = await fetch('http://127.0.0.1:8090/api/collections/productos');
-  const data = await res.json();
-  console.log(JSON.stringify(data.schema, null, 2));
-}
-
-checkSchema().catch(console.error);
