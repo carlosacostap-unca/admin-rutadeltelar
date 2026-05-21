a15e36d17f1cd49e231a9b01ea08efeb83eacac9
carlosacostap-unca <carlosacostap-unca@users.noreply.github.com>
2026-05-21 15:35:41 -0300

    Use satellite map tiles

--- components/MapPickerInner.tsx
+++ components/MapPickerInner.tsx
@@ -4,6 +4,7 @@ import { useMemo } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+import { SATELLITE_TILE_LAYER } from "./mapTiles";
 
 export interface MapPickerProps {
   lat: number | null;
@@ -71,8 +72,8 @@ export default function MapPicker({ lat, lng, zoom = 13, label = "Ubicación sel
         style={{ height: "100%", width: "100%", zIndex: 0 }}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution={SATELLITE_TILE_LAYER.attribution}
+          url={SATELLITE_TILE_LAYER.url}
         />
         <LocationMarker lat={hasValidCoordinates ? lat : null} lng={hasValidCoordinates ? lng : null} onLocationSelect={onLocationSelect} label={label} icon={icon} />
       </MapContainer>
---
+++ components/mapTiles.ts
@@ -0,0 +1,5 @@
+export const SATELLITE_TILE_LAYER = {
+  url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+  attribution:
+    'Tiles &copy; Esri, Maxar, Earthstar Geographics, and the GIS User Community',
+};
